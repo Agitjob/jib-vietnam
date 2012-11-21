f303b207546efaa069e2cf7f4538a394d3774e3f
Evelyn Huynh <ehuynh@agamatrix.com>
2012-11-22 02:49:04 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JibVietnam::Application.config.secret_token = 'c2d41969edd60060138ffa12c8712367005fbee06a79469be8d19ee7c08bc7a56ddd5fd9862d16651c80188414233ed7933826d13468885ca8f9fa9d35d9c4d0'
